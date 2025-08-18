8bd06e1a3ba0c6bc113be6369f12fd629ed4a382
AlvaroCareli <122116260+alvarosamp@users.noreply.github.com>
2025-08-18 20:40:58 -0300

    Atualizacao

--- c214_projeto/src/projetoUm/api.py
+++ c214_projeto/src/projetoUm/api.py
@@ -1,12 +1,28 @@
+from urllib import response
 import requests
 
 # Exemplo: API pública de piadas
 url = "https://official-joke-api.appspot.com/random_joke"
 
-response = requests.get(url)
 
-if response.status_code == 200:
-    data = response.json()
-    print(f"{data['setup']} - {data['punchline']}")
-else:
-    print("Erro ao acessar a API:", response.status_code)
+def teste_12():
+    if response.status_code == 200:
+        data = response.json()
+        print(f"{data['setup']} - {data['punchline']}")
+    else:
+        print("Erro ao acessar a API:", response.status_code)
+
+def teste_22():
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if "setup" in data and "punchline" in data:
+            print("✅ Teste passou: API retornou piada válida.")
+        else:
+            print("❌ Teste falhou: resposta não contém os campos esperados.")
+    else:
+        print("❌ Teste falhou: erro ao acessar API. Status:", response.status_code)
+
+
+teste_12()
+teste_22()
